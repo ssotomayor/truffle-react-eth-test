15987968027535f9f7796eb4c93b1a84e1a9a6d6
Santiago Sotomayor <sansoto2003@yahoo.com.ar>
2019-04-17 18:05:40 -0300

    App logic to check

---
+++ README.md
@@ -0,0 +1,36 @@
+## Installation
+
+1. Install node.js (version 8.9.x)
+
+1. Install webpack globally
+    ```
+    npm install -g webpack
+    npm install -g webpack-cli
+    ```
+
+1. Install truffle and Ganache, both available from [Truffle](https://truffleframework.com/).
+
+1. Compile and migrate the contracts.
+    ```javascript
+    truffle compile
+    truffle migrate
+    ```
+
+1. Run the webpack server for front-end hot reloading. For now, smart contract changes must be manually recompiled and migrated.
+    ```javascript
+    npm start
+    ```
+
+## Docker
+
+1. Build the docker container.
+    ```
+    docker build -t etherslist .
+    ```
+
+1. Launch the docker container.
+    ```
+    docker run -p 3000:3000 etherslist:latest
+    ```
+
+1. Browse to http://localhost:3000
---
+++ contracts/Merchandise.sol
@@ -0,0 +1,105 @@
+pragma solidity 0.5.0;  // Update to Solidity 0.5
+
+contract Merchandise {
+    address owner;
+    bool online;
+    struct Item {
+        uint itemId;
+        address seller;
+        address buyer;
+        string itemName;
+        string itemDesc;
+        uint itemPrice;
+        bool sold;
+        bool shipped;
+        bool received;
+    }
+
+    Item[] public items;
+
+    constructor() public {
+        owner = msg.sender;
+        online = true;
+        // addItem("First Item", "Added by Contract deployment", 1 ether);
+    }
+
+    function kill() public {
+        require(msg.sender == owner, "Only contract owner can kill contract");
+        selfdestruct(msg.sender); // Can only transfer funds to "address payable" types, so use msg.sender here
+    }
+
+    function setOnline(bool status) public {
+        require(msg.sender == owner, "Only contract owner can set online status");
+        online = status;
+    }
+
+    event AddListing(
+        address seller,
+        string name,
+        uint price
+    );
+
+    function addItem(string memory name, string memory description, uint price) public returns(uint) { 
+        // string storage   ^^ location must now be explicitly declared
+        require(online == true, "Store must be online to add items");
+        require(price > 0, "Price must be greater than zero");
+        uint initialLength = items.length;
+        items.push(Item({
+            itemId: items.length,
+            seller: msg.sender,
+            buyer: address(0),
+            itemName: name,
+            itemDesc: description,
+            itemPrice: price,
+            sold: false,
+            shipped: false,
+            received: false
+        }));
+        emit AddListing(msg.sender, name, price);
+        assert(items.length == initialLength + 1);
+        return items.length;
+    }
+
+    function getItem(uint itemId) public view returns(
+        uint, string memory, string memory, uint, bool, bool, bool
+        // string storage location  ^^ must be explicitly declared
+    ) {
+        return (
+            items[itemId].itemId,
+            items[itemId].itemName,
+            items[itemId].itemDesc,
+            items[itemId].itemPrice,
+            items[itemId].sold,
+            items[itemId].shipped,
+            items[itemId].received
+        );
+    }
+
+    function buyItem(uint itemId) public payable returns(bool) {
+        require(online = true, "Store must be online to buy items");
+        require(msg.value == items[itemId].itemPrice, "Eth submitted must equal purchase price");
+        require(items[itemId].sold != true, "Cannot purchase items already sold");
+        items[itemId].buyer = msg.sender;
+        items[itemId].sold = true;
+        return true;
+    }
+
+    function shipItem(uint itemId) public {
+        require(items[itemId].sold == true, "Cannot ship unsold items");
+        items[itemId].shipped = true;
+    }
+
+    function receiveItem(uint itemId) public {
+        require(items[itemId].shipped == true, "Cannot receive unsold items");
+        require(msg.sender == items[itemId].buyer, "Only buyer can mark item as received");
+        items[itemId].received = true;
+    }
+
+    function claimFunds(uint itemId) public {
+        require(online == true, "Store must be online to claim funds");
+        require(msg.sender == items[itemId].seller, "Only seller can claim funds");
+        require(items[itemId].received == true, "Cannot claim funds until item received");
+        msg.sender.transfer(items[itemId].itemPrice);
+    }
+
+}
